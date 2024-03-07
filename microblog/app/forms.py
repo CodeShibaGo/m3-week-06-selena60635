e57bf786eda5c17fa2400934d0d753ad01d6f498
selena60635 <selena606352000@gmail.com>
2024-03-08 00:29:28 +0800

    add login form

---
+++ microblog/app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('使用者名', validators=[DataRequired()])
+    password = PasswordField('密碼', validators=[DataRequired()])
+    remember_me = BooleanField('記住我')
+    submit = SubmitField('登入')
