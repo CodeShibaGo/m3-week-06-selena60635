9f5fb0152d9ddc8dde09eedc933be4a130c53f35
selena60635 <selena606352000@gmail.com>
2024-03-07 23:47:31 +0800

    first template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Selena'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Portland 的天氣真好！'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': '復仇者聯盟電影真的很酷！'
+        }
+    ]
+    return render_template('index.html', title='首頁', user=user, posts=posts)
+
