49110c0422f28bef8be2e892f3057b5e16590d0e
selena60635 <selena606352000@gmail.com>
2024-03-09 16:27:01 +0800

    finish user register

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import app, db
@@ -40,3 +40,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('恭喜，你現在是一名註冊使用者！')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
